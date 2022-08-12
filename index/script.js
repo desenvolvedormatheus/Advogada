286b4fc687ccf1afd02479929c5b3302c619f434
Matheus Pereira de Souza <matheus.souza.smm@gmail.com>
2022-08-12 12:58:22 -0300

    0.5

--- index/script.js
+++ index/script.js
@@ -1,4 +1,12 @@
 function abrirmenu() {
     var btnmenu = document.querySelector('.btnmobile')
     btnmenu.style.transform = 'rotate(180deg)'
+    var menu2 = document.querySelector('.menu2')
+    menu2.style.right = '2rem'
+}
+function fecharmenu() {
+    var btnmenu = document.querySelector('.btnmobile')
+    btnmenu.style.transform = 'rotate(90deg)'
+    var menu2 = document.querySelector('.menu2')
+    menu2.style.right = '-8rem'
 }
